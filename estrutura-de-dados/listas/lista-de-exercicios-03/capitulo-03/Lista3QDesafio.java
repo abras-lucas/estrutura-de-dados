44ae9a6cb5a227666037b5fe256b38d582fdb865
Abrahão Lucas Barboza Nunes <45404200+abras-lucas@users.noreply.github.com>
2019-01-07 21:51:35 -0200

    Lista de exercicios-03

---
+++ estrutura-de-dados/listas/lista-de-exercicios-03/capitulo-03/Lista3QDesafio.java
@@ -0,0 +1,11 @@
+class Lista3QDesafio{
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+    	while (b<=100){
+      		System.out.print(a+" "+b+" ");
+      		a+=b;
+      		b+=a;
+    	}	
+  	}
+}
