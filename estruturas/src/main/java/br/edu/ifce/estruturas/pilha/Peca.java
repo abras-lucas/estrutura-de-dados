299ed26325dca2005d3d3e9a2beacbe62f911401
Abrahão Lucas Barboza Nunes <45404200+abras-lucas@users.noreply.github.com>
2019-03-18 21:47:21 -0300

    Add files via upload

---
+++ estruturas/src/main/java/br/edu/ifce/estruturas/pilha/Peca.java
@@ -0,0 +1,22 @@
+class Peca{
+
+	private String nome;
+
+	public String getNome(){
+
+		return nome;
+
+	}
+
+	public void setNome(String nome){
+
+		this.nome = nome;
+
+	}
+	
+	public String toString() {
+		
+		return this.nome;
+		
+	}
+}
